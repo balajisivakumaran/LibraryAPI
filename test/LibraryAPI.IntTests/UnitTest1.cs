a7bfc0a0bfef06f9ac7e9c5d9d29ac21dec56611
balaji <balajisivakumaran@yahoo.co.in>
2023-08-21 15:58:23 +1200

    Initial commit

---
+++ test/LibraryAPI.IntTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LibraryAPI.IntTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/LibraryAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LibraryAPI.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
